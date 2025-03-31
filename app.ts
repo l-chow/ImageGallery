e9308299cc824c4305a9ba39db581d1a6e7cc847
Laura Chow <l.chow.tech@gmail.com>
2025-03-31 16:26:13 -0700

    Added ability to like each individual image. New API endpoint and data for each image stored in JSON file in lieu of database

--- app.ts
+++ app.ts
@@ -5,6 +5,12 @@ const fs = require("fs");
 
 const app = express();
 
+type Image = {
+  id: number;
+  filename: string;
+  likes: number;
+};
+
 // app.get("/", (req, res) => {
 //   res.sendFile(__dirname + "/index.html");
 // });
@@ -12,15 +18,70 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get("/images", (req, res) => {
-  fs.readdir(__dirname + "/images", (err, files) => {
+  // fs.readdir(__dirname + "/images", (err, files) => {
+  //   let response = [];
+  //   for (let i = 0; i < files.length; i++) {
+  //     response.push({
+  //       filename: files[i],
+  //     });
+  //   }
+  //   res.status(200).json(response);
+  // });
+  fs.readFile(__dirname + "/data/images.json", (err, data) => {
     let response = [];
-    for (let i = 0; i < files.length; i++) {
-      response.push({
-        filename: files[i],
+    const imgarr: Image[] = JSON.parse(data);
+    if (imgarr) {
+      imgarr.forEach((img) => {
+        response.push({
+          id: img.id,
+          filename: img.filename,
+          likes: img.likes,
+        });
       });
+      res.status(200).json(response);
+    } else {
+      res.status(404).json({});
     }
-    res.status(200).json(response);
   });
+});
+
+app.post("/images/like/:id", (req, res) => {
+  let imgid = req.params.id;
+  console.log("hi api");
+  if (imgid) {
+    fs.readFile(__dirname + "/data/images.json", (err, data) => {
+      console.log("read file");
+      let imgarr: Image[] = JSON.parse(data);
+      let toReturn: Image;
+      if (imgarr) {
+        let newImgArr = imgarr.map((img) => {
+          if (img.id == imgid) {
+            toReturn = {
+              id: img.id,
+              filename: img.filename,
+              likes: img.likes + 1,
+            };
+            return toReturn;
+          } else {
+            return img;
+          }
+        });
+        console.log("writing file");
+        fs.writeFile(
+          __dirname + "/data/images.json",
+          JSON.stringify(newImgArr),
+          { flag: "w+" },
+          (err) => {
+            console.log(err);
+          }
+        );
+        res.status(200).json({
+          status: "success",
+          data: toReturn,
+        });
+      }
+    });
+  }
 });
 
 app.listen(3000, () => {
--- gallery.js
+++ gallery.js
@@ -6,7 +6,8 @@ carouselItemTemplate.appendChild(carouselImageTemplate);
 
 window.onload = () => {
   setCarouselItems();
-  console.log("test");
+  handleCarouselInteraction();
+  handleLikeButtonClicked();
 };
 
 const setCarouselItems = async () => {
@@ -15,13 +16,21 @@ const setCarouselItems = async () => {
     const images = await response.json();
     images.forEach((image, index) => {
       let newCarouselItem = carouselItemTemplate.cloneNode(true);
-      if (index == 0) newCarouselItem.classList.add("active");
+      if (index == 0) {
+        newCarouselItem.classList.add("active");
+        document.getElementById("button-like").querySelector("span").innerHTML =
+          image.likes;
+      }
       newCarouselItem
         .querySelector("img")
         .setAttribute("src", "/images/" + image.filename);
       newCarouselItem
         .querySelector("img")
         .setAttribute("alt", image.filename.split(".")[0]);
+      newCarouselItem.querySelector("img").setAttribute("data-id", image.id);
+      newCarouselItem
+        .querySelector("img")
+        .setAttribute("data-likes", image.likes);
       document
         .getElementById("image-carousel")
         .querySelector("div")
@@ -34,4 +43,47 @@ const setCarouselItems = async () => {
   } catch (error) {
     console.log(error);
   }
+};
+
+const likeImage = () => {
+  const image = document
+    .getElementById("image-carousel")
+    .getElementsByClassName("carousel-item active")[0]
+    .querySelector("img");
+  if (image) {
+    console.log(image);
+
+    let imgid = image.dataset.id;
+    fetch("/images/like/" + imgid, {
+      method: "POST",
+      body: "",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.status == "success") {
+          image.dataset.likes = json.data.likes;
+          document
+            .getElementById("button-like")
+            .querySelector("span").innerHTML = json.data.likes;
+        }
+      });
+  }
+};
+
+const handleCarouselInteraction = () => {
+  const carousel = document.getElementById("image-carousel");
+  carousel.addEventListener("slid.bs.carousel", (event) => {
+    let item = event.relatedTarget;
+    let img = item.querySelector("img");
+    document.getElementById("span-like-count").innerHTML = img.dataset.likes;
+  });
+};
+
+const handleLikeButtonClicked = () => {
+  document.getElementById("button-like").addEventListener("click", (e) => {
+    likeImage();
+  });
 };
